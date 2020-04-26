6d09cd449b5826ed065a7e35ecec47c7613dd555
JSellers0 <JSellers0@Gmail.Com>
2020-04-26 09:41:53 -0400

    add bcrypt for password verification

--- api.py
+++ api.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 import config
 
 connex_app = config.connex_app
+bcrypt = Bcrypt
+bcrypt.init_app(connex_app)
 
 connex_app.add_api("swagger.yml")
 
