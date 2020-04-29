a199a855d7d681b84ddf57bbf660868f03c297db
JSellers0 <JSellers0@Gmail.Com>
2020-04-29 16:14:47 -0400

    Add test data on rebuild

--- build_database.py
+++ build_database.py
@@ -1,5 +1,5 @@
 import os
-from config import db
+from config import db, bc
 from models import User, Client, Project, Timelog
 
 if os.path.exists("timeclock.sqlite"):
@@ -10,3 +10,19 @@ db.create_all()
 os.chown("timeclock.sqlite", 48, 48)
 
 os.chmod("timeclock.sqlite", 0o664)
+
+# Add some test data
+user = User(
+    user_name="user1",
+    email="user1@test.com",
+    encoded_password=bc.generate_password_hash("user1").decode("utf-8")
+)
+
+client = Client(client_name="Test Client")
+
+project = Project(project_name="Test Project")
+
+db.session.add(user)
+db.session.add(client)
+db.session.add(project)
+db.session.commit()
