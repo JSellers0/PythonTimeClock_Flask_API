695e8a2375a5e76801a06eb8d4976bf8cd3e9817
JSellers0 <JSellers0@Gmail.Com>
2020-05-01 04:48:13 -0400

    remove useless methods; debug daterange

--- timelog.py
+++ timelog.py
@@ -1,12 +1,15 @@
 """
 This is the Timelog module and supports all REST actions for the timelog table
 """
+import json
 from datetime import datetime as dt
 from flask import abort
-from models import Timelog, TimelogSchema, Client, Project
+from models import Timelog, TimelogSchema, Project, Task, Note
 from config import db
+from sqlalchemy import or_
 
 def read_user_rows(userid):
+    userid = userid.get("userid")
     timelogs = Timelog.query.filter(Timelog.userid == userid).order_by(Timelog.timelogid).all()
 
     if len(timelogs ) == 0:
@@ -19,7 +22,8 @@ def read_user_rows(userid):
         data = timelog_schema.dump(timelogs)
         return data, 200
 
-def create(userid, timelog):
+def create(timelog):
+    userid = timelog.get("userid")
     start = dt.strptime(timelog.get("start"), "%Y-%m-%d %H:%M:%S")
     stop = timelog.get("stop")
 
@@ -44,14 +48,14 @@ def create(userid, timelog):
         if stop == "na":
             new_timelog = Timelog(
                 userid=timelog.get("userid"),
-                clientid=timelog.get("clientid"),
+                taskid=timelog.get("taskid"),
                 projectid=timelog.get("projectid"),
                 start=start
             )
         else:
             new_timelog = Timelog(
                 userid=timelog.get("userid"),
-                clientid=timelog.get("clientid"),
+                taskid=timelog.get("taskid"),
                 projectid=timelog.get("projectid"),
                 start=start,
                 stop=stop
@@ -65,78 +69,8 @@ def create(userid, timelog):
     else:
         abort(
             409,
-            ("Attempting to duplicate Timelog {tlid} start ands top for {userid}."
+            ("Attempting to duplicate Timelog {tlid} start and stop for {userid}."
             .format(tlid=existing_timelog.timelogid, userid=userid)
-            )
-        )
-        
-def read_user_current_row(userid):
-    timelog = (
-        Timelog.query
-        .filter(Timelog.userid == userid)
-        .order_by(Timelog.timelogid.desc())
-        .first()
-    )
-
-    if timelog is not None:
-        timelog_schema = TimelogSchema()
-        data = timelog_schema.dump(timelog)
-        return data, 200
-    else:
-        abort(
-            404,
-            "Current Timelog Record not found for {}".format(userid)
-        )
-
-def update_user_current_row(userid, timelog):
-    update_timelog = (
-        Timelog.query
-        .filter(Timelog.userid == userid)
-        .order_by(Timelog.timelogid.desc())
-        .first()
-    )
-
-    if update_timelog is None:
-        abort(
-            404,
-            "Current Timelog Row not found for user {}".format(userid)
-        )
-    else:
-        schema = TimelogSchema()
-        update = schema.load(timelog, session=db.session)
-
-        db.session.merge(update)
-        db.session.commit()
-
-        data = schema.dump(update_timelog)
-
-        return data, 200
-        
-def read_user_rows_daterange(userid, daterange):
-    range_start = daterange.get("range_start")
-    range_end = daterange.get("range_end")
-
-    timelogs = (
-        Timelog.query.filter(
-            Timelog.userid == userid
-        ).filter(
-            Timelog.start < range_end
-        ).filter(or_(
-            Timelog.end > range_start,
-            Timelog.end == None
-            )
-        )
-    )
-
-    if timelogs is not None:
-        timelog_schema = TimelogSchema(many=True)
-        data = timelog_schema.dump(timelogs)
-        return data, 200
-    else:
-        abort(
-            404,
-            "Timelogs not found for {userid} between {start} and {end}".format(
-                userid=userid, start=range_start, end=range_end
             )
         )
 
@@ -168,19 +102,70 @@ def read_row_detail(timelogid):
             "No row found for {}".format(timelogid)
         )
     else:
-        client = Client.query.filter(Client.clientid == timelog.clientid).one_or_none()
+        task = Task.query.filter(Task.taskid == timelog.taskid).one_or_none()
         project = Project.query.filter(Project.projectid == timelog.projectid).one_or_none()
+        note = Note.query.filter(Note.noteid == timelog.noteid).one_or_none()
         timelog_dump = {
             "timelogid": str(timelog.timelogid),
             "userid": str(timelog.userid),
-            "clientid": str(timelog.clientid),
-            "client_name": client.client_name,
+            "taskid": str(timelog.taskid),
+            "task_name": task.task_name,
             "projectid": str(timelog.projectid),
             "project_name": project.project_name,
-            "start": timelog.start.strftime("%Y-%m-%d %H:%M:%S")
+            "noteid": str(note.noteid),
+            "note_name": note.note_name,
+            "start": timelog.start.strftime("%Y-%m-%d %H:%M:%S"),
+            "stop": timelog.stop.strftime("%Y-%m-%d %H:%M:%S")
         }
-        return timelog_dump, 200
+        return json.dumps(timelog_dump), 200
 
+def read_daterange(userid, range_begin, range_end):
+    range_start = dt.strptime(range_begin, "\"%Y-%m-%d %H:%M:%S\"")
+    range_end = dt.strptime(range_end, "\"%Y-%m-%d %H:%M:%S\"")
+
+    timelogs = (
+        Timelog.query.filter(
+            Timelog.userid == userid
+        ).filter(
+            Timelog.start < range_end
+        ).filter(or_(
+            Timelog.stop > range_start,
+            Timelog.stop == None
+            )
+        )
+    )
+
+    if [timelog for timelog in timelogs]:
+        timelog_dump = {"rows": []}
+        for timelog in timelogs:
+            task = Task.query.filter(Task.taskid == timelog.taskid).one_or_none()
+            project = Project.query.filter(Project.projectid == timelog.projectid).one_or_none()
+            note = Note.query.filter(Note.noteid == timelog.noteid).one_or_none()
+            timelog_row = {
+                "timelogid": str(timelog.timelogid),
+                "userid": str(timelog.userid),
+                "taskid": str(timelog.taskid),
+                "task_name": task.task_name,
+                "projectid": str(timelog.projectid),
+                "project_name": project.project_name,
+                "noteid": str(note.noteid),
+                "note_name": note.note_name,
+                "start": timelog.start.strftime("%Y-%m-%d %H:%M:%S"),
+                "stop": timelog.stop.strftime("%Y-%m-%d %H:%M:%S")
+            }
+            timelog_dump["rows"].append(timelog_row)
+
+            print(timelog_dump)
+
+        return json.dumps(timelog_dump), 200
+
+    else:
+        abort(
+            404,
+            "Timelogs not found for {userid} between {start} and {end}".format(
+                userid=userid, start=range_start, end=range_end
+            )
+        )
 
 
 
