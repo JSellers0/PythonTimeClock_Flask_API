f661465c02c7fc37944696dcf4f0f4c91895189e
JSellers0 <JSellers0@Gmail.com>
2021-01-23 12:35:28 -0600

    paramaterize connections n shit

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import connexion
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
+from myshit import s_mysql, s_host
 
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
@@ -12,7 +13,7 @@ connex_app = connexion.App(__name__, specification_dir=basedir)
 # Get the Flask app instance
 app = connex_app.app
 
-db_uri = "mysql://clock:" + "M4xG3*@jgOIRdEgmjUfSV" + "@localhost/clock"
+db_uri = "mysql://clock:" + s_mysql + "@" + s_host + "/clock"
 
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
