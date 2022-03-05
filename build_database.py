bcfaf78edf6740a21e3385588339196aac15124b
Sellerman <Jeffrey.Sellers@maritz.com>
2022-03-05 12:39:46 -0500

    comment out testing stuff until I can toggle.

--- build_database.py
+++ build_database.py
@@ -7,15 +7,15 @@ from datetime import datetime as dt
 
 # ToDo: Add debug option to toggle test user and log creation.
 
-if os.path.exists("timeclock.sqlite"):
-    os.remove("timeclock.sqlite")
+# if os.path.exists("timeclock.sqlite"):
+#     os.remove("timeclock.sqlite")
 
 db.create_all()
 
-if "MacBook" not in socket.gethostname():
-    os.chown("timeclock.sqlite", 48, 48)
+# if "MacBook" not in socket.gethostname():
+#     os.chown("timeclock.sqlite", 48, 48)
 
-    os.chmod("timeclock.sqlite", 0o664)
+#     os.chmod("timeclock.sqlite", 0o664)
 
 # General Overhead Projects, Tasks, and Notes
 db.session.add(Project(project_name="general overhead"))
@@ -31,37 +31,33 @@ db.session.add(Task(task_name="mgr approved pd"))
 db.session.add(Task(task_name="sick pd"))
 db.session.add(Task(task_name="vacation pd"))
 
-# Show Work Tasks
-db.session.add(Task(task_name="reg-app dev 110.005"))
-db.session.add(Task(task_name="ebs-general 230.001"))
-
 # Commit real data in case we don't need test data
 db.session.commit()
 
-# Add some test data
-db.session.add(User(
-    user_name="user1",
-    email="user1@test.com",
-    encoded_password=bc.generate_password_hash("user1").decode("utf-8")
-))
+# # Add some test data
+# db.session.add(User(
+#     user_name="user1",
+#     email="user1@test.com",
+#     encoded_password=bc.generate_password_hash("user1").decode("utf-8")
+# ))
 
-db.session.add(Timelog(
-    userid=1,
-    projectid=1,
-    taskid=1,
-    noteid=1,
-    start=dt.strptime("2020-05-31 12:00:00", "%Y-%m-%d %H:%M:%S"),
-    stop=dt.strptime("2020-05-31 13:00:00", "%Y-%m-%d %H:%M:%S")
-))
+# db.session.add(Timelog(
+#     userid=1,
+#     projectid=1,
+#     taskid=1,
+#     noteid=1,
+#     start=dt.strptime("2020-05-31 12:00:00", "%Y-%m-%d %H:%M:%S"),
+#     stop=dt.strptime("2020-05-31 13:00:00", "%Y-%m-%d %H:%M:%S")
+# ))
 
-db.session.add(Timelog(
-    userid=1,
-    projectid=1,
-    taskid=1,
-    noteid=1,
-    start=dt.strptime("2020-05-31 14:00:00", "%Y-%m-%d %H:%M:%S"),
-    stop=dt.strptime("2020-05-31 16:00:00", "%Y-%m-%d %H:%M:%S")
-))
+# db.session.add(Timelog(
+#     userid=1,
+#     projectid=1,
+#     taskid=1,
+#     noteid=1,
+#     start=dt.strptime("2020-05-31 14:00:00", "%Y-%m-%d %H:%M:%S"),
+#     stop=dt.strptime("2020-05-31 16:00:00", "%Y-%m-%d %H:%M:%S")
+# ))
 
-# Commit Test Data
-db.session.commit()
+# # Commit Test Data
+# db.session.commit()
