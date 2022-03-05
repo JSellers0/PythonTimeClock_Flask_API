116e6ebd7e947017247acbc0a430ea176f5e0a44
Sellerman <Jeffrey.Sellers@maritz.com>
2022-03-05 12:39:29 -0500

    use dotenv instead of permanent environment variables

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ import connexion
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
-from myshit import s_mysql, s_host
+from dotenv import load_dotenv
+
+load_dotenv()
 
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 
@@ -13,7 +15,7 @@ connex_app = connexion.App(__name__, specification_dir=basedir)
 # Get the Flask app instance
 app = connex_app.app
 
-db_uri = "mysql://clock:" + s_mysql + "@" + s_host + "/clock"
+db_uri = "mysql://clock:" + os.getenv('PYCLOCK_MYSQL_USER') + "@" + os.getenv("PYCLOCK_MYSQL_HOST") + "/clock"
 
 app.config["SQLALCHEMY_ECHO"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
